775751a6af69f0bf537113d80e75f778a62ee184
Monika Szucs <monika.silvia.s@hotmail.ca>
2022-05-13 21:58:11 -0700

    Finished Creating Converter and converted into Executable file
    created an executable file that acts as a desktop icon to click on.

--- src/main/java/com/converter/TempConverter.java
+++ src/main/java/com/converter/TempConverter.java
@@ -8,14 +8,20 @@ import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import java.awt.Color;
 import javax.swing.SwingConstants;
+
+import com.formdev.flatlaf.IntelliJTheme;
+
 import java.awt.Font;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
 
 public class TempConverter {
 
 	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -83,8 +89,49 @@ public class TempConverter {
 				System.exit(0);
 			}
 		});
-		btnNewButton.setEnabled(false);
+		btnNewButton.setEnabled(true);
 		btnNewButton.setBounds(350, 0, 50, 50);
 		panel_1.add(btnNewButton);
+		
+		textField = new JTextField();
+		textField.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		textField.setHorizontalAlignment(SwingConstants.CENTER);
+		textField.setBounds(40, 153, 280, 33);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setEditable(false);
+		textField_1.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		textField_1.setHorizontalAlignment(SwingConstants.CENTER);
+		textField_1.setColumns(10);
+		textField_1.setBounds(40, 284, 280, 33);
+		panel_1.add(textField_1);
+		
+		JButton btnNewButton_1 = new JButton("Convert");
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				double f = Double.valueOf(textField.getText());
+				double c = (f-32) * ((double)5/9);
+				textField_1.setText(String.valueOf(c));
+			}
+		});
+		btnNewButton_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		btnNewButton_1.setBounds(40, 220, 280, 33);
+		panel_1.add(btnNewButton_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("\u00B0F");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_2.setForeground(Color.WHITE);
+		lblNewLabel_2.setBounds(330, 153, 37, 33);
+		panel_1.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("\u00B0C");
+		lblNewLabel_2_1.setHorizontalAlignment(SwingConstants.CENTER);
+		lblNewLabel_2_1.setForeground(Color.WHITE);
+		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_2_1.setBounds(330, 284, 37, 33);
+		panel_1.add(lblNewLabel_2_1);
 	}
 }
